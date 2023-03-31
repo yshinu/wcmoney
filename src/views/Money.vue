97f6b99e22296ca1280d19118ff21f67bd3bb681
admin <1443880381@qq.com>
2023-03-31 14:15:35 +0800

    money的页面布局

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,7 +1,11 @@
 <template>
-    <div>
+    <div class="nav-wrapper">
+        <div class="content">
+            money.vue
+
+        </div>
         <Nav/>
-        money.vue
+
     </div>
 
 </template>
@@ -16,5 +20,16 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.nav-wrapper{
+    border: 1px solid green;
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+}
+.content{
+    overflow: auto;
+    border: 1px solid yellow;
+    flex-grow: 1;
+}
 
 </style>
