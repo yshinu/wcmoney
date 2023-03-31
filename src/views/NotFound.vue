214d442d91f5f53d9e07b94bdc0bfc0e2089253d
admin <1443880381@qq.com>
2023-03-31 14:07:47 +0800

    添加404

---
+++ src/views/NotFound.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <router-link to="/money">点击返回主页</router-link>
+        <div>404 当前页面不存在，请检查网址是否正确</div>
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+    name: "NotFound"
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
