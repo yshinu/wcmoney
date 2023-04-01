ecc273e0cdbcb028789f0748c6c5b305596f78a4
admin <1443880381@qq.com>
2023-04-01 12:24:39 +0800

    import导入整个目录报错了，所以就一个个引入

---
+++ src/components/Layout.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="nav-wrapper">
+        <div class="content">
+          <slot></slot>
+        </div>
+        <Nav/>
+    </div>
+</template>
+
+
+
+<style lang="scss" scoped>
+.nav-wrapper {
+  border: 1px solid green;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
+
+.content {
+  overflow: auto;
+  border: 1px solid yellow;
+  flex-grow: 1;
+}
+
+</style>
+<script setup lang="ts">
+</script>
