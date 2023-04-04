7a6842a33399db1a75893af33353b0ddc375a246
admin <1443880381@qq.com>
2023-04-04 16:44:01 +0800

    添加 EditLabel.vue 的样式，抽离了两个组件 Button 和 FormItem

---
+++ src/components/Button.vue
@@ -0,0 +1,23 @@
+<template>
+    <button class="button">
+        <slot/>
+    </button>
+</template>
+
+<script lang="ts">
+import {Component, Vue} from "vue-property-decorator";
+@Component
+export default class Button extends Vue {
+}
+</script>
+
+<style lang="scss" scoped>
+.button {
+    background: #767676;
+    color: white;
+    border: none;
+    border-radius: 3px;
+    height: 40px;
+    padding: 0 16px;
+}
+</style>
